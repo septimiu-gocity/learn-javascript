a5ce0e72d9646f6f4311163f0a43ac235731c3c4
Septimiu-Iulian Calin <septimiu.calin@gocity.com>
2023-09-08 12:35:39 +0100

    first 9 lessons

--- 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
+++ 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
@@ -1,4 +1,24 @@
 // 1. Create two variables, firstCard and secondCard. 
 // Set their values to a random number between 2-11
 
+let firstCard = 4
+let secondCard = 7
+
 // 2. Create a variable, sum, and set it to the sum of the two cards
+
+let sum = firstCard + secondCard;
+
+if (sum < 21) {
+    console.log("Draw a new card?");
+} else if (sum === 21){
+    console.log("Congrats you won the game!");
+} else {
+    console.log("You're out!");
+}
+
+let age = 22;
+
+if (age <= 21) console.log("You cant enter the club");
+else console.log("Have fun!");
+
+console.log(sum)
